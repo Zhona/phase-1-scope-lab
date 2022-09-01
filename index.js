9065e0186c6eb929ea056b509af86dcd97051f14
Teana Zhong <Teanawk@gmail.com>
2022-09-01 16:10:19 -0400

    commit tree b9e60447fd0c50fdcfb7b67de7062e4bb7e7b79c

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob' // this is a global scope because we didnt use var, let or const
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = 'me '
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not me"
+}
